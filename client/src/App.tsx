daad35c72f8ac37a08a3d05ba37fffd90e6efbf0
Rafael Singer <rafaelsinger66@gmail.com>
2022-07-20 01:02:57 -0700

    initialized react frontend w/ ts and bootstrap

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+
+function App() {
+  return (
+    <div>
+      <Fragment>hello</Fragment>
+      <Button>yes</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/ListTodos.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListTodos = () => {
+  return (
+    <div>ListTodos</div>
+  )
+}
+
+export default ListTodos
